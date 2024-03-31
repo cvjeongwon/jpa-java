a3774716ca4dd8268baf631eabec9154a1b7e646
cvjeongwon <cvjeongwon@gmail.com>
2024-03-31 11:07:25 +0900

    -- JPA 실습 --  entity >  Locker, Member, Team -- resources > persistence.xml show tables; desc team; desc locker; desc membertbl; -- entity > Member.java 에서 연관관계 매핑 : 팀에는 여러 멤머가 있고 팀과 락커는 1:1 관계이다.
    -- japexam1> app> MemberTeamTest1 실행 합니다.
    -- db consol 에서 테이블 조회 합니다.
    select * from team;
    select * from  locker;
    select * from membertbl;

--- src/main/java/jpaexam1/entity/Member.java
+++ src/main/java/jpaexam1/entity/Member.java
@@ -16,6 +16,7 @@ public class Member {
 	private int id;
 	private String username;
 	// 연관관계 매핑 : 팀에는 여러 멤머가 있고 팀과 락커는 1:1 관계이다.
+	// 연관관계 매핑 : 팀에는 여러 멤머가 있고 팀과 락커는 1:1 관계이다.
 	@ManyToOne
 	@JoinColumn(name = "TEAM_ID") // 테이블의 컬럼명 'TEAM_ID'
 	private Team team;  // team_TEAM_ID
